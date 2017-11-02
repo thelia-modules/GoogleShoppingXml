f59df9a3a2d989a03aa6fa1e56e62b17657683dc
Théophile Pumain <tpumain@openstudio.fr>
2017-11-02 10:04:38 +0100

    Perform log pagination in SQL (not JS anymore)

--- Controller/LogController.php
+++ Controller/LogController.php
@@ -16,13 +16,31 @@ class LogController extends BaseAdminController
             return $response;
         }
 
-        $limit = $this->getRequest()->get('limit', 50);
-        $offset = $this->getRequest()->get('offset', null);
+        /** @var \Thelia\Core\HttpFoundation\Request $request **/
+        $request = $this->getRequest();
 
+        $limit = $request->get('limit', 50);
+        $offset = $request->get('offset', null);
+        $levels_checked = [];
+
+        if ($request->get('info', null) == 1) $levels_checked[] = GoogleshoppingxmlLogQuery::LEVEL_INFORMATION;
+        if ($request->get('success', null) == 1) $levels_checked[] = GoogleshoppingxmlLogQuery::LEVEL_SUCCESS;
+        if ($request->get('warning', null) == 1) $levels_checked[] = GoogleshoppingxmlLogQuery::LEVEL_WARNING;
+        if ($request->get('error', null) == 1) $levels_checked[] = GoogleshoppingxmlLogQuery::LEVEL_ERROR;
+        if ($request->get('fatal', null) == 1) $levels_checked[] = GoogleshoppingxmlLogQuery::LEVEL_FATAL;
+
+        /** @var GoogleshoppingxmlLogQuery $query **/
         $query = GoogleshoppingxmlLogQuery::create()
             ->orderByCreatedAt('desc')
             ->orderById('desc')
             ->limit($limit);
+
+        for ($i = 0; $i < count($levels_checked); $i++) {
+            if ($i > 0) {
+                $query->_or();
+            }
+            $query->filterByLevel($levels_checked[$i]);
+        }
 
         if (!empty($offset)) {
             $query->offset($offset);
