0218988923bdaca5f69aeb9132c46523a07f92b8
ArthurLashermes <alashermes@MacBookPro-de-Arthur.local>
2022-10-24 09:48:41 +0200

    Add filter on feed on categories

---
+++ Model/GoogleshoppingxmlIgnoreCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GoogleShoppingXml\Model;
+
+use GoogleShoppingXml\Model\Base\GoogleshoppingxmlIgnoreCategory as BaseGoogleshoppingxmlIgnoreCategory;
+
+/**
+ * Skeleton subclass for representing a row from the 'googleshoppingxml_ignore_category' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class GoogleshoppingxmlIgnoreCategory extends BaseGoogleshoppingxmlIgnoreCategory
+{
+
+}
