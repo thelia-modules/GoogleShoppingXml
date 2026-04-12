a1482978091086d461b8a8e86dfab220801f403f
Alexandre Nozière <anoziere@openstudio.fr>
2026-04-12 21:21:03 +0200

    Fix Symfony 7.4 compatibility
    GenerateXmlFileCommand.php:execute;

--- Command/GenerateXmlFileCommand.php
+++ Command/GenerateXmlFileCommand.php
@@ -49,7 +49,7 @@ class GenerateXmlFileCommand extends ContainerAwareCommand
     /**
      * @throws Exception
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->initRequest();
 
