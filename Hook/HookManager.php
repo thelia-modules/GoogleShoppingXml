a7382f047aedf3736ba47d559e3ccf9e38e41bf9
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-09 22:40:37 +0200

    feat: migrate and modernize back-office for Twig

--- Hook/HookManager.php
+++ Hook/HookManager.php
@@ -5,15 +5,37 @@ namespace GoogleShoppingXml\Hook;
 use Thelia\Core\Event\Hook\HookRenderEvent;
 use Thelia\Core\Hook\BaseHook;
 
-/**
- * Class HookManager
- * @package GoogleShoppingXml\Hook
- */
 class HookManager extends BaseHook
 {
-    public function onMainHeadCss(HookRenderEvent $event)
+    public static function getSubscribedHooks(): array
     {
-        $content = $this->addCSS('css/style.css');
-        $event->add($content);
+        return [
+            'main.head-css' => [
+                ['type' => 'back', 'method' => 'onMainHeadCss'],
+            ],
+            'home.bottom' => [
+                ['type' => 'back', 'method' => 'onHomeBottom'],
+            ],
+            'home.js' => [
+                ['type' => 'back', 'method' => 'onHomeJs'],
+            ],
+        ];
+    }
+
+    public function onMainHeadCss(HookRenderEvent $event): void
+    {
+        // addCSS() relies on the current parser, which can be null in the Twig BO.
+        // Emit the module styles from a dedicated fragment instead.
+        $event->add($this->render('GoogleShoppingXml/main-head-css.html.twig'));
+    }
+
+    public function onHomeBottom(HookRenderEvent $event): void
+    {
+        $event->add($this->render('GoogleShoppingXml/home-bottom.html.twig'));
+    }
+
+    public function onHomeJs(HookRenderEvent $event): void
+    {
+        $event->add($this->render('GoogleShoppingXml/home-js.html.twig'));
     }
 }
